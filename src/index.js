4b53d35931efdaf0e77a3ffc946070a4e1b2f523
Dawid Płocki <dawid.plocki@gmail.com>
2017-05-20 12:23:34 +0200

    Adding router for API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+var config = require('./config'); // get our config file
 var express = require('express')
+var mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(config.database)
+
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var router = express.Router()
+router.route('/api')
+
+router.get('/', function(req, res) {
+  res.send('Yep, that is API')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/', function(req, res) {
+    res.send('Hello! The API is at http://localhost:' + PORT + '/api')
+})
+
+app.use('/api', router)
+
+app.listen(PORT, function() {
+  console.log('Magic happens at http://localhost:' + PORT)
 })
